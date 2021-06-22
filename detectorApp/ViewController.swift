e1f3b989bca707ebb1613787e2ed935c31b41b66
BlueSaNGG <>
2021-06-22 16:11:48 +0800

    first commit

---
+++ detectorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  detectorApp
+//
+//  Created by jinqi on 2021/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
